7f005f4b19b1b4ac045a43db2d78e0d4de3b2933
毕康 <bikang1@staff.weibo.com>
2018-04-10 20:10:47 +0800

    go

--- file.go
+++ file.go
@@ -122,18 +122,23 @@ func NewFileWriter(filename string, cfg FileRotationConfig) (io.Writer, error) {
 
 func (f *file) Level() LEVEL { return f.level }
 
+//日志行数的分隔符号
 var newLineBytes = []byte("\n")
 
+//文件初始化, 把日志写入到文件
 func (f *file) initFile() (err error) {
+	//打开文件
 	f.file, err = os.OpenFile(f.filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660)
+	//返回错误
 	if err != nil {
 		return fmt.Errorf("OpenFile '%s': %v", f.filename, err)
 	}
-
+	//新建一个日志处理类，基本的处理，加上日期-时间
 	f.Logger = log.New(f.file, "", log.Ldate|log.Ltime)
 	return nil
 }
 
+//判断路径是否存在
 // isExist checks whether a file or directory exists.
 // It returns false when the file or directory does not exist.
 func isExist(path string) bool {
@@ -141,13 +146,16 @@ func isExist(path string) bool {
 	return err == nil || os.IsExist(err)
 }
 
+//获取rotate文件的名字
 // rotateFilename returns next available rotate filename with given date.
 func (f *file) rotateFilename(date string) string {
 	filename := fmt.Sprintf("%s.%s", f.filename, date)
+	//不存在直接返回文件名
 	if !isExist(filename) {
 		return filename
 	}
 
+	//如果存在文件，则在后面添加..
 	format := filename + ".%03d"
 	for i := 1; i < 1000; i++ {
 		filename := fmt.Sprintf(format, i)
@@ -159,6 +167,10 @@ func (f *file) rotateFilename(date string) string {
 	panic("too many log files for yesterday")
 }
 
+/**
+删除超过时间是文件
+对一个目录下的文件执行同样的操作
+**/
 func (f *file) deleteOutdatedFiles() {
 	filepath.Walk(filepath.Dir(f.filename), func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() &&
@@ -170,37 +182,45 @@ func (f *file) deleteOutdatedFiles() {
 	})
 }
 
+//分文件配置
 func (f *file) initRotate() error {
 	// Gather basic file info for rotation.
+	//获取文件的状态
 	fi, err := f.file.Stat()
 	if err != nil {
 		return fmt.Errorf("Stat: %v", err)
 	}
-
+	//当前大小
 	f.currentSize = fi.Size()
 
+	//如果最大行，和当前行都大于0
 	// If there is any content in the file, count the number of lines.
 	if f.rotate.MaxLines > 0 && f.currentSize > 0 {
+		//读取当前文件
 		data, err := ioutil.ReadFile(f.filename)
 		if err != nil {
 			return fmt.Errorf("ReadFile '%s': %v", f.filename, err)
 		}
-
+		//计算文件的行数
 		f.currentLines = int64(bytes.Count(data, newLineBytes)) + 1
 	}
 
+	//是否按照天去分文件
 	if f.rotate.Daily {
+		//获取当前的时间
 		now := time.Now()
+		//获取当前的日期
 		f.openDay = now.Day()
-
+		//最后的时间
 		lastWriteTime := fi.ModTime()
 		if lastWriteTime.Year() != now.Year() ||
 			lastWriteTime.Month() != now.Month() ||
 			lastWriteTime.Day() != now.Day() {
-
+			//关闭文件
 			if err = f.file.Close(); err != nil {
 				return fmt.Errorf("Close: %v", err)
 			}
+			//对当前文件重命名为自旋的文件名
 			if err = os.Rename(f.filename, f.rotateFilename(lastWriteTime.Format(SIMPLE_DATE_FORMAT))); err != nil {
 				return fmt.Errorf("Rename: %v", err)
 			}
@@ -211,6 +231,7 @@ func (f *file) initRotate() error {
 		}
 	}
 
+	//MaxDays > 0 删除超过日期的文件
 	if f.rotate.MaxDays > 0 {
 		f.deleteOutdatedFiles()
 	}
@@ -232,16 +253,18 @@ func (f *file) Init(v interface{}) (err error) {
 
 	//文件基本名
 	f.filename = cfg.Filename
+	//创建文件夹，如果不存在，否则返回错误
 	os.MkdirAll(filepath.Dir(f.filename), os.ModePerm)
 	if err = f.initFile(); err != nil {
 		return fmt.Errorf("initFile: %v", err)
 	}
-
+	//基本的rotate配置
 	f.rotate = cfg.FileRotationConfig
+	//分文件的话，开启份文件配置
 	if f.rotate.Rotate {
 		f.initRotate()
 	}
-
+	//如果不是独立的则创建chan
 	if !f.standalone {
 		f.msgChan = make(chan *Message, cfg.BufferSize)
 	}
@@ -254,43 +277,58 @@ func (f *file) ExchangeChans(errorChan chan<- error) chan *Message {
 	return f.msgChan
 }
 
+//写日志
 func (f *file) write(msg *Message) int {
+	//打印消息体
 	f.Logger.Print(msg.Body)
 
+	//消息的总长度
 	bytesWrote := len(msg.Body)
+
 	if !f.standalone {
+		//时间的长度
 		bytesWrote += LOG_PREFIX_LENGTH
 	}
+
+	//是否写入多个文件
 	if f.rotate.Rotate {
+		//记录消息的字节数
 		f.currentSize += int64(bytesWrote)
+		//记录消息行数
 		f.currentLines++ // TODO: should I care if log message itself contains new lines?
 
+		//是否需要分文件，分文件时间
 		var (
 			needsRotate = false
 			rotateDate  time.Time
 		)
-
+		//当前时间
 		now := time.Now()
+		//根据文件的打开日期和当前时间判断是否需要分文件
 		if f.rotate.Daily && now.Day() != f.openDay {
 			needsRotate = true
 			rotateDate = now.Add(-24 * time.Hour)
 
+			//超过长度，超过字节
 		} else if (f.rotate.MaxSize > 0 && f.currentSize >= f.rotate.MaxSize) ||
 			(f.rotate.MaxLines > 0 && f.currentLines >= f.rotate.MaxLines) {
 			needsRotate = true
 			rotateDate = now
 		}
 
+		//需要分文件
 		if needsRotate {
+			//关闭文件
 			f.file.Close()
+			//重新命名
 			if err := os.Rename(f.filename, f.rotateFilename(rotateDate.Format(SIMPLE_DATE_FORMAT))); err != nil {
 				f.errorChan <- fmt.Errorf("fail to rename rotate file '%s': %v", f.filename, err)
 			}
-
+			//打开文件
 			if err := f.initFile(); err != nil {
 				f.errorChan <- fmt.Errorf("fail to init log file '%s': %v", f.filename, err)
 			}
-
+			//写now.Day
 			f.openDay = now.Day()
 			f.currentSize = 0
 			f.currentLines = 0
